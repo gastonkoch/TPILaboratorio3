befb491024907c0a02691ae3e7950b8bb40dd88c
GASTON KOCH <GASTON.KOCH@hotmail.com>
2024-06-22 16:24:57 -0300

    Carrito

--- TPILaboratorio3/src/components/carrito/Carrito.jsx
+++ TPILaboratorio3/src/components/carrito/Carrito.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Button, ListGroup } from 'react-bootstrap';
+import { Button, Form, ListGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../services/cart/CartContext';
 import './Carrito.css';
@@ -7,29 +7,47 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import PropTypes from "prop-types";
-
+import lapiz from '/public/lapiz.png';
+import cruz from '/public/cruz2.png';
+import check from '/public/check3.png';
 
 const Carrito = ({ setCartProducts }) => {
   const [showPayMethod, setShowPayMethod] = useState(false);
   const { handleProduct } = useContext(CartContext);
   const [products, setProducts] = useState([]);
-  
+  const [editableIndex, setEditableIndex] = useState(null);
+  const [newQuantity, setNewQuantity] = useState(0);
+
   const navigate = useNavigate();
 
   useEffect(() => {
     setProducts(handleProduct());
   }, [handleProduct]);
 
+  const handleUpdateProduct = (index) => {
+    setEditableIndex(index);
+    setNewQuantity(products[index].quantity);
+  };
+
   const handleRemoveProduct = (index) => {
     const newProducts = [...products];
     newProducts.splice(index, 1);
     setProducts(newProducts);
     setCartProducts(newProducts);
+
+  };
+
+  const handleSaveProduct = (index) => {
+    const updatedProducts = [...products];
+    updatedProducts[index].quantity = newQuantity;
+    setProducts(updatedProducts);
+    setCartProducts(updatedProducts);
+    setEditableIndex(null);
   };
 
   const handlePay = () => {
-    if(products.length === 0){
-      alert("Debe agregar productos al carrito")
+    if (products.length === 0) {
+      alert("Debe agregar productos al carrito");
     } else {
       navigate('/paymethod');
     }
@@ -45,15 +63,43 @@ const Carrito = ({ setCartProducts }) => {
         {products.length === 0 ? (
           <p>El carrito está vacío</p>
         ) : (
-          <Container>
-            {products.map((product, index) => (
-              <Row key={index} className='d-flex justify-content-between align-items-center product'>
-                <Col><span>{product.name}</span></Col>
-                <Col><span>${product.price.toFixed(2)}</span></Col>
-                <Col><span>{product.quantity}</span></Col>
-                <Col><Button variant='danger' size='sm' onClick={() => handleRemoveProduct(index)}>X</Button></Col>
-              </Row>
-            ))}
+          <Container className='container-cart'>
+            <Form className='form-cart'>
+              {products.map((product, index) => (
+                <Row key={index} className='d-flex justify-content-between align-items-center product'>
+                  <Col><span>{product.name}</span></Col>
+                  <Col><span>${product.price.toFixed(2)}</span></Col>
+                  <Col>
+                    {editableIndex === index ? (
+                      <Form.Control
+                        type="number"
+                        value={newQuantity}
+                        onChange={(e) => setNewQuantity(e.target.value)}
+                        className='quantity-update-input'
+                      />
+                    ) : (
+                      <Form.Control type="text" value={product.quantity} readOnly />
+                    )}
+                  </Col>
+                  <Col>
+                    {editableIndex === index ? (
+                      <Button className='check-cart' size='sm' onClick={() => handleSaveProduct(index)}>
+                        <img className="icono-grilla" src={check} alt="delete" />
+                      </Button>
+                    ) : (
+                      <Button className='update-cart' size='sm' onClick={() => handleUpdateProduct(index)}>
+                        <img className="icono-grilla" src={lapiz} alt="update" />
+                      </Button>
+                    )}
+                  </Col>
+                  <Col>
+                    <Button variant='danger' size='sm' onClick={() => handleRemoveProduct(index)}>
+                      <img className="icono-grilla" src={cruz} alt="delete" />
+                    </Button>
+                  </Col>
+                </Row>
+              ))}
+            </Form>
           </Container>
         )}
       </div>
@@ -67,6 +113,5 @@ const Carrito = ({ setCartProducts }) => {
 Carrito.propTypes = {
   setCartProducts: PropTypes.func
 };
-
 
 export default Carrito;
